580a0601f9f7e2ff843398f37ed8d0785923de13
PassiHD <passihd@phoenixts.eu>
2024-06-16 20:36:07 +0200

    feat: stuff
    Signed-off-by: PassiHD <passihd@phoenixts.eu>

--- assets/script/flipit.js
+++ assets/script/flipit.js
@@ -1,10 +1,10 @@
 function flip() {    
     Array.prototype.slice.call(document.querySelectorAll('body')).map(function(tag){tag.style['transform'] = 'rotate(' + 180 +'deg)';});
-    var fieldNameElement = document.getElementById('lbb');
+    var fieldNameElement = document.getElementById('flip');
     fieldNameElement.onclick = function () { unflip() };
 }
 function unflip() {    
     Array.prototype.slice.call(document.querySelectorAll('body')).map(function(tag){tag.style['transform'] = 'rotate(' + 0 +'deg)';});
-    var fieldNameElement = document.getElementById('lbb');
+    var fieldNameElement = document.getElementById('flip');
     fieldNameElement.onclick = function () { flip() };
 }
