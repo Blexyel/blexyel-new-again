f1dc8379150f680a64ad190a6d60f4abc15b5b15
PassiHD <passihd@phoenixts.eu>
2024-07-28 05:19:39 +0200

    feat: yea
    Signed-off-by: PassiHD <passihd@phoenixts.eu>

--- assets/script/status.js
+++
@@ -1,8 +0,0 @@
-function statusreload() {
-    const interval = setInterval(
-        function actualreload() {
-            document.getElementById('status').src = document.getElementById('status').src;
-            clearInterval(interval);
-        }, 5000
-    );
-}
